6e05d4e200d0d79f8bd031266bbc6ec39e74ccbd
Kevin Fechner <bkevinfechner1989@gmail.com>
2017-01-29 18:36:46 +0100

    add css

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ io.on('connection', (socket) => {
 		console.log('Create Message:', message);
 		// to every 
 		io.emit('newMessage', generateMessage(message.from, message.text));
-		callback('This is from the server');
+		callback();
 	});
 
 	socket.on('createLocationMessage', (coords) => {
